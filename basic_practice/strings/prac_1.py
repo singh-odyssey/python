c3222cd22b7b966ac9435a31f2b9d164765d25a3
Aditya Singh <148507838+singh-odyssey@users.noreply.github.com>
2025-04-26 17:59:25 +0000

    Intial commit

---
+++ basic_practice/strings/prac_1.py
@@ -0,0 +1,4 @@
+#prog to greet user on there input name
+
+name=input("Enter your name -> ")
+print(f"Hi {name} \n How are you")
---
+++ basic_practice/strings/prac_4.py
@@ -0,0 +1,5 @@
+# replacing double spaces with single space in user given string 
+
+user_input = input("Enter your string : -> ")
+update_string = user_input.replace("  ", " ")
+print(update_string)
